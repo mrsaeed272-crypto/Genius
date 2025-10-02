3f34342caee0358ad4b25eeaa212905dd5a01980
mrsaeed272-crypto <mrsaeed272@gmail.com>
2025-10-02 20:41:18 +0500

    Create App.jsx

---
+++ src/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Upload from './components/Upload'
+
+function App() {
+  return (
+    <div>
+      <h1 style={{ textAlign: 'center', color: 'white', backgroundColor: 'black', padding: '10px' }}>
+        Genius - AI Background Remover
+      </h1>
+      <Upload />
+    </div>
+  )
+}
+
+export default App
